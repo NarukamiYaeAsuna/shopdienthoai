2f3addd4a13e949b486552bbca462a65b41e9bcf
BIT230132 NGUYEN VAN GIANG <157077469+NarukamiYaeAsuna@users.noreply.github.com>
2024-10-15 09:47:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function openGiohang(){
+    var giohang = document.getElementById('giohang');
+    giohang.classList.toggle('open');
+
+}
+function closeGiohang(){
+    var giohang = document.getElementById('giohang');
+    
+    giohang.classList.remove('open');
+   }
+function list(){
+    var sohang = document.getElementById('sohang')
+    sohang.classList.toggle('open')
+}
